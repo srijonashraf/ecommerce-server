95ffdecf5218a893c281edc0857349cf16dd90f6
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-20 22:42:10 +0600

    updated

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     userId: { type: String, default: () => uuidv4().slice(0, 8), unique: true },
     avatar: { type: String },
     name: { type: String },
-    email: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true, lowercase: true },
     password: { type: String },
     mobile: { type: String },
     otp: { type: String },
