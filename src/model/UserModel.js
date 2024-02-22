9c612d637f6c630db0699256ae38c70bd7e531dd
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-22 20:10:14 +0600

    model updated

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 const UserSchema = new mongoose.Schema(
   {
     userId: { type: String, default: () => uuidv4().slice(0, 8), unique: true },
-    avatar: { type: String },
-    name: { type: String },
+    name: { type: String, default: "" },
+    avatar: { type: String, default: "" },
     email: { type: String, unique: true, required: true, lowercase: true },
-    password: { type: String },
-    mobile: { type: String },
-    otp: { type: String },
+    password: { type: String, default: "" },
+    mobile: { type: String, default: "" },
+    otp: { type: String, default: "" },
   },
   {
     timestamps: true,
